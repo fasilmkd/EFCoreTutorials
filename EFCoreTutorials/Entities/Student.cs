9c16241586837f35f061b9fc7c82f842b7621bc4
Mohammed Fasil <mohammed.fasil@codinghands.in>
2022-10-07 17:36:03 +0530

    Student

---
+++ EFCoreTutorials/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreTutorials.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ EFCoreTutorials/SchoolContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreTutorials
+{
+    public class SchoolContext : DbContext
+    {
+
+    }
+}
